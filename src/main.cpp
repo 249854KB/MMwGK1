9868aeddef688e3b6966947cf6d4a9c329c5b4f6
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-11-26 22:15:50 +0100

    1. Zadanko done

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
+
 #include <GL/freeglut.h>
 
-void display()
-{
+int currentView = 1;  // Default to the first view
+void displayDefaultView() {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_TRIANGLES);
+     
+
+    // Triangle 1 (Blue)
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(-0.25f, -0.25f, 0.0f);
+    glVertex3f(0.25f, -0.25f, 0.0f);
+
+    // Triangle 2 (Red)
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(-0.25f, -0.25f, 0.0f);
+    glVertex3f(0.0f, +0.25f, 0.0f);
+
+    // Triangle 3 (Green)
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(0.25f, -0.25f, 0.0f);
+    glVertex3f(0.0f, +0.25f, 0.0f);
+
+    glEnd();
+
+    glFlush();
+}
+
+void displayRightView() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+    glBegin(GL_TRIANGLES);
     glBegin(GL_TRIANGLES);
 
     // Triangle 1 (Blue)
@@ -26,12 +64,29 @@ void display()
 
     glEnd();
 
-    // glutSwapBuffers();
-    glFlush(); // Required in single buffering
+    glFlush();
 }
 
-int main(int argc, char **argv)
-{
+void display() {
+    if (currentView == 1) {
+        displayDefaultView();
+    } else if (currentView == 2) {
+        displayRightView();
+    }
+
+    glutSwapBuffers();
+}
+void keyboard(unsigned char key, int x, int y) {
+    if (key == '1') {
+        currentView = 1;
+    } else if (key == '2') {
+        currentView = 2;
+    }
+
+    glutPostRedisplay();
+}
+
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
@@ -43,6 +98,7 @@ int main(int argc, char **argv)
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Set orthographic projection
 
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
     glutMainLoop();
 
     return 0;
