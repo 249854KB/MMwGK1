f578b689ca164df768c617f33dda137269510add
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-11-26 22:05:27 +0100

    added glut

--- src/main.cpp
+++ src/main.cpp
@@ -1,183 +1,49 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <GL/freeglut.h>
 
-// Shader sources
-const char *vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 projection;
-
-    void main()
-    {
-        gl_Position = projection * view * model * vec4(aPos, 1.0);
-    }
-)";
-
-const char *fragmentShaderSource = R"(
-    #version 330 core
-    out vec4 FragColor;
-
-    uniform vec4 color;
-
-    void main()
-    {
-        FragColor = color;
-    }
-)";
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
-
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-int main()
+void display()
 {
-   if (!glfwInit())
-    {
-        std::cout << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Zadanie 1", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
+    glBegin(GL_TRIANGLES);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    // Triangle 1 (Blue)
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(-0.25f, -0.25f, 0.0f);
+    glVertex3f(0.25f, -0.25f, 0.0f);
 
-   // Set up the vertex data
-    float vertices[] = {
-        // Triangle 1 (Blue)
-        0.0f, 1.0f, 0.0f, // Top
-        -0.5f, 0.0f, -0.5f, // Bottom-left
-        0.5f, 0.0f, -0.5f, // Bottom-right
+    // Triangle 2 (Red)
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(-0.25f, -0.25f, 0.0f);
+    glVertex3f(0.0f, +0.25f, 0.0f);
 
-        // Triangle 2 (Green)
-        0.0f, 1.0f, 0.0f, // Top
-        0.5f, 0.0f, -0.5f, // Bottom-left
-        0.5f, 0.0f, 0.5f, // Bottom-right
+    // Triangle 3 (Green)
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.25f);
+    glVertex3f(0.25f, -0.25f, 0.0f);
+    glVertex3f(0.0f, +0.25f, 0.0f);
 
-        // Triangle 3 (Yellow)
-        0.0f, 1.0f, 0.0f, // Top
-        0.5f, 0.0f, 0.5f, // Bottom-left
-        -0.5f, 0.0f, 0.5f // Bottom-right
-    };
+    glEnd();
 
+    // glutSwapBuffers();
+    glFlush(); // Required in single buffering
+}
 
-    // Create Vertex Buffer Object, Vertex Array Object, and Shader Program
-    unsigned int VBO, VAO, vertexShader, fragmentShader, shaderProgram;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("MMwGK - Lab 1");
 
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set clear color
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Set orthographic projection
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Vertex attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    // Create shaders and link the program
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Check for shader compile/link errors here...
-
-    // Cleanup shaders (you may want to keep them if debugging shaders)
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
- // Set up projection matrix
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-    // Set up view matrix
-    glm::mat4 view = glm::lookAt(glm::vec3(1.5f, 1.5f, 1.5f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
-    // Rendering loop
-    // ---------------
-    while (!glfwWindowShouldClose(window))
-    {
-        // Input handling
-        processInput(window);
-
-        // Rendering
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Use the shader program
-        glUseProgram(shaderProgram);
-
- 	// Set shader uniforms
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(glm::mat4(1.0f)));
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
-        // Draw the triangles
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 9);
-
-        // Check and call events and swap the buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // Cleanup
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    glfwTerminate();
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
-}
-
-// Proccessing inputs
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// Callback function for window resizing
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    // Set the new viewport size
-    glViewport(0, 0, width, height);
 }
