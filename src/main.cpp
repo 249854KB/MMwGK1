1ca756a582d3a291fcbefc134c9b219018271130
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-11-27 10:01:11 +0100

    Lab 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,108 @@
 #include <GL/freeglut.h>
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
+GLfloat m_transX = 0;
+GLfloat m_transY = 0;
+GLfloat m_angle1 = 0;
+GLfloat m_angle2 = 0;
+GLuint ArmPart = 0;
 
-    glBegin(GL_TRIANGLES);
+GLfloat m_LeftDownPos[2] = {0.0, 0.0};
+GLfloat m_RightDownPos[2] = {0.0, 0.0};
+GLboolean m_LeftButtonDown = GL_FALSE;
+GLboolean m_RightButtonDown = GL_FALSE;
 
-    // Vertex 1: Red
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex2f(-0.5, -0.5);
+void myPaint() {
+    glPushMatrix();
+    glTranslatef(m_transX, m_transY, 0);
+    glRotatef(m_angle1, 0, 0, 1);
 
-    // Vertex 2: Green
-    glColor3f(0.0, 1.0, 0.0);
-    glVertex2f(0.5, -0.5);
+    glPushMatrix();
+    glTranslatef(90, 0, 0);
+    glRotatef(m_angle2, 0, 0, 1);
+    glColor4f(0.0, 1.0, 0.0, 1.0);
+    glCallList(ArmPart);
+    glPopMatrix();
 
-    // Vertex 3: Blue
-    glColor3f(0.0, 0.0, 1.0);
-    glVertex2f(0.0, 0.5);
-
-    glEnd();
-
-    glFlush();
+    glColor4f(1.0, 0.0, 0.0, 1.0);
+    glCallList(ArmPart);
+    glPopMatrix();
 }
 
-void init() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+void drawArmPart() {
+    glBegin(GL_POLYGON);
+    glVertex2f(-10.0, 10.0);
+    glVertex2f(-10.0, -10.0);
+    glVertex2f(100.0, -10.0);
+    glVertex2f(100.0, 10.0);
+    glEnd();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    myPaint();
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+    gluOrtho2D(-500.0 * (w / (float)h), 500.0 * (w / (float)h), -500.0 * (w / (float)h), 500.0);
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, w, h);
+}
+
+void motion(int x, int y) {
+    if (m_LeftButtonDown) {
+        m_angle1 += m_LeftDownPos[0] - x;
+        m_angle2 += m_LeftDownPos[1] - y;
+        m_LeftDownPos[0] = x;
+        m_LeftDownPos[1] = y;
+    } else if (m_RightButtonDown) {
+        m_transX -= m_RightDownPos[0] - x;
+        m_transY += m_RightDownPos[1] - y;
+        m_RightDownPos[0] = x;
+        m_RightDownPos[1] = y;
+    }
+    glutPostRedisplay();  // Request a redraw
+}
+
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            m_LeftButtonDown = GL_TRUE;
+            m_LeftDownPos[0] = x;
+            m_LeftDownPos[1] = y;
+        } else if (state == GLUT_UP) {
+            m_LeftButtonDown = GL_FALSE;
+        }
+    } else if (button == GLUT_RIGHT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            m_RightButtonDown = GL_TRUE;
+            m_RightDownPos[0] = x;
+            m_RightDownPos[1] = y;
+        } else if (state == GLUT_UP) {
+            m_RightButtonDown = GL_FALSE;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(400, 400);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Multi-Color Triangle");
-
-    init();
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutCreateWindow("Double Pendulum");
+    glutReshapeFunc(reshape);
     glutDisplayFunc(display);
+    glutMotionFunc(motion);
+    glutMouseFunc(mouse);
+
+    ArmPart = glGenLists(1);
+    glNewList(ArmPart, GL_COMPILE);
+    drawArmPart();
+    glEndList();
+
     glutMainLoop();
 
     return 0;
 }
 
-// Wszystkie kolory automatycznie zmieszały się na zasadzie gradietnu. Efekt końcowy w pliku Efekt-koncowy-1.png lub po uruchomieniu programu build/myprogram.exe
+//Udało się dodać płynną animację ruchu ręki. Dodawanie inputów w postaci myszki jest bardzo intuicyjne w opengl.
