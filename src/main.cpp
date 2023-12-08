892681399363bc78f37e49737f948f592e7f0173
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-12-08 17:42:46 +0100

    Troche lepiej

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,97 @@
+
+#include <unistd.h>
+#include <vector>
 #include <GL/freeglut.h>
 
-GLfloat m_transX = 0;
-GLfloat m_transY = 0;
-GLfloat m_angle1 = 0;
-GLfloat m_angle2 = 0;
-GLuint ArmPart = 0;
+struct Point {
+    float x;
+    float y;
+};
 
-GLfloat m_LeftDownPos[2] = {0.0, 0.0};
-GLfloat m_RightDownPos[2] = {0.0, 0.0};
-GLboolean m_LeftButtonDown = GL_FALSE;
-GLboolean m_RightButtonDown = GL_FALSE;
+struct Triangle {
+    float x;
+    float y;
+    float color[4]; // Color
+};
 
-void myPaint() {
-    glPushMatrix();
-    glTranslatef(m_transX, m_transY, 0);
-    glRotatef(m_angle1, 0, 0, 1);
+int degree = 0;
+const float speed = 1.5;
+float rate = 1000000 / 60;
 
-    glPushMatrix();
-    glTranslatef(90, 0, 0);
-    glRotatef(m_angle2, 0, 0, 1);
-    glColor4f(0.0, 1.0, 0.0, 1.0);
-    glCallList(ArmPart);
-    glPopMatrix();
+Point points[3] = {
+    Point{-1.0, -1.0},
+    Point{1.0, -1.0},
+    Point{-1.0, 1.0}
+};
 
-    glColor4f(1.0, 0.0, 0.0, 1.0);
-    glCallList(ArmPart);
-    glPopMatrix();
+std::vector<std::vector<Triangle>> triangles = {
+    {
+        Triangle{1, 3, {0.5, 0.2, 0.8, 1.0}}, // purple
+        Triangle{-1, -1, {0.8, 0.7, 0.2, 1.0}}// yellow
+        
+    },
+    {
+        Triangle{1, 1, {0.2, 0.5, 0.8, 1.0}}, // light blue
+        Triangle{3, 1, {0.8, 0.1, 0.1, 1.0}},  // dark red
+        // Triangle{2, 2, {0.9, 0.8, 0.2, 1.0}},  // light yellow
+        // Triangle{3, 1, {0.8, 0.2, 0.5, 1.0}}  // pinkish
+    }
+};
+
+void show() {
+    double rotation = (int)(degree * speed) % 360;
+    degree += 1;
+    double distance = abs(rotation / 360.0 * 2 - 1);
+
+    glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for (int r = 0; r < 2; r++) {
+        rotation = -rotation;
+
+        for (int i = 0; i < 4; i++) {//4 razy rysujemy to samo tylko obrócone
+            glPushMatrix();
+            glRotated(90 * i, 0, 0, 1); 
+            glRotated(-rotation, 0, 0, 1);
+            glTranslated(0.5 * distance, 0.5 * distance, 0);
+
+            for (Triangle triangle : triangles[r]) {
+                glPushMatrix();
+                glTranslated(triangle.x, triangle.y, 0);
+                glTranslated(triangle.x * distance, triangle.y * distance, 0);
+                glRotated(rotation * 2, 0, 0, 1);
+                glColor4fv(triangle.color);
+
+                glBegin(GL_POLYGON);
+                glVertex2f(points[0].x, points[0].y);
+                glVertex2f(points[1].x, points[1].y);
+                glVertex2f(points[2].x, points[2].y);
+                glEnd();
+
+                glPopMatrix();
+            }
+            glPopMatrix();
+        }
+    }
+
+    usleep(rate);
+    glutPostRedisplay();
+    glFlush();
 }
 
-void drawArmPart() {
-    glBegin(GL_POLYGON);
-    glVertex2f(-10.0, 10.0);
-    glVertex2f(-10.0, -10.0);
-    glVertex2f(100.0, -10.0);
-    glVertex2f(100.0, 10.0);
-    glEnd();
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    myPaint();
-    glutSwapBuffers();
-}
-
-void reshape(int w, int h) {
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Lab 3");
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glViewport(0, 0, 800, 800);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(-500.0 * (w / (float)h), 500.0 * (w / (float)h), -500.0 * (w / (float)h), 500.0);
+    gluOrtho2D(-10, 10, -10, 10);
     glMatrixMode(GL_MODELVIEW);
-    glViewport(0, 0, w, h);
-}
-
-void motion(int x, int y) {
-    if (m_LeftButtonDown) {
-        m_angle1 += m_LeftDownPos[0] - x;
-        m_angle2 += m_LeftDownPos[1] - y;
-        m_LeftDownPos[0] = x;
-        m_LeftDownPos[1] = y;
-    } else if (m_RightButtonDown) {
-        m_transX -= m_RightDownPos[0] - x;
-        m_transY += m_RightDownPos[1] - y;
-        m_RightDownPos[0] = x;
-        m_RightDownPos[1] = y;
-    }
-    glutPostRedisplay();  // Request a redraw
-}
-
-void mouse(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON) {
-        if (state == GLUT_DOWN) {
-            m_LeftButtonDown = GL_TRUE;
-            m_LeftDownPos[0] = x;
-            m_LeftDownPos[1] = y;
-        } else if (state == GLUT_UP) {
-            m_LeftButtonDown = GL_FALSE;
-        }
-    } else if (button == GLUT_RIGHT_BUTTON) {
-        if (state == GLUT_DOWN) {
-            m_RightButtonDown = GL_TRUE;
-            m_RightDownPos[0] = x;
-            m_RightDownPos[1] = y;
-        } else if (state == GLUT_UP) {
-            m_RightButtonDown = GL_FALSE;
-        }
-    }
-}
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutCreateWindow("Double Pendulum");
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutMotionFunc(motion);
-    glutMouseFunc(mouse);
-
-    ArmPart = glGenLists(1);
-    glNewList(ArmPart, GL_COMPILE);
-    drawArmPart();
-    glEndList();
-
+    glutDisplayFunc(show);
     glutMainLoop();
-
     return 0;
 }
-
-//Udało się dodać płynną animację ruchu ręki. Dodawanie inputów w postaci myszki jest bardzo intuicyjne w opengl.
